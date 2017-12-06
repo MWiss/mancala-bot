684dd7f63c032885c1b33477fdf8f08e703a5a1b
Mark Wissink <mwissink.b@gmail.com>
2017-12-06 00:00:19 -0500

    Add project files.

---
+++ mancala/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mancala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
