3c73405c9fb943daf5f1ea0079de2acfb1aa86e4
Mark Wissink <mwissink.b@gmail.com>
2017-12-12 16:36:49 -0500

    better comments on functions

--- mancala/mcw33Player.cs
+++ mancala/mcw33Player.cs
@@ -28,6 +28,7 @@ namespace Mancala
         public mcw33Player(Position pos, int timeLimit) : base(pos, "mcw33", timeLimit)
         {
             position = pos;
+            // Store offsets so we don't have to constantly check if player is on top or bottom
             offsets = (position == Position.Top) ? Tuple.Create(7, 0) : Tuple.Create(0, 7);
         }
 
@@ -53,17 +54,21 @@ namespace Mancala
             });
 
             // Run the threads
-            Tuple<int, int> res = Tuple.Create(-1, 0);
+            Tuple<int, int> result = Tuple.Create(-1, 0); // Item1 = the best move, Item2 = the score of the best move
             try
             {
+                // Spawn 100 threads, more than enough to find optimal move at any point in game
                 Parallel.For(1, 100, po, i =>
                 {
-                    res = minimaxVal(b, i, int.MinValue, int.MaxValue, po);
+                    result = minimaxVal(b, i, int.MinValue, int.MaxValue, po);
                 });
             }
+            // Catch threads that are cancelled
             catch (OperationCanceledException) { }
             finally { cts.Dispose(); }
-            return res.Item1;
+
+            // Return the last value that was assigned to result
+            return result.Item1;
         }
 
         public Tuple<int, int> minimaxVal(Board b, int d, int alpha, int beta, ParallelOptions po)
@@ -74,13 +79,14 @@ namespace Mancala
 
             // Cancel process if time is up
             po.CancellationToken.ThrowIfCancellationRequested();
-
-            // Minimaxing
+            
+            // Initalize variables to make minimax code shorter
             bool myTurn = b.whoseMove() == position;
             int bestMove = -1;
             int offset = myTurn ? offsets.Item1 : offsets.Item2;
             int bestVal = myTurn ? int.MinValue : int.MaxValue;
 
+            // Minimaxing
             for (int move = 0 + offset; move <= 5 + offset; move++)
             {
                 if (b.legalMove(move))
@@ -104,16 +110,21 @@ namespace Mancala
             return Tuple.Create(bestMove, bestVal);
         }
 
+        /* Evaluates board based on:
+         * - the difference between number of stones in mancalas and each pit
+         * - the difference between my and my opponents potential capture scores for each pit
+         * - the difference between my and my opponents potential go-again scores for each pit
+         */
         public override int evaluate(Board b)
         {
             int eval = 0;
-            // Check scores of mancalas
+            // Check difference between scores of mancalas
             eval += (b.stonesAt(6 + offsets.Item1) - b.stonesAt(6 + offsets.Item2)) * weights["mancala"];
             for (int i = 0; i <= 5; i++)
             {
                 int myPit = i + offsets.Item1;
                 int enemyPit = i + offsets.Item2;
-                // Add all other conditions to the eval
+                // Add all other conditions to the eval - pits, potential captures, and potential go-agains
                 eval += ((b.stonesAt(myPit) - b.stonesAt(enemyPit)) * weights["pit"])
                      + ((checkCapture(b, myPit, offsets.Item1) - checkCapture(b, enemyPit, offsets.Item2)) * weights["capture"])
                      + ((goAgain(b, myPit, offsets.Item1) - goAgain(b, enemyPit, offsets.Item2)) * weights["turn"]);
@@ -121,19 +132,30 @@ namespace Mancala
             return eval;
         }
 
+        /* Check for potential capture next turn
+         * Conditions for a capture:
+         * - last stone drop has to land in empty pit
+         * - pit has to be on our side
+         */
         public int checkCapture(Board b, int pit, int offset)
         {
-            // Check for potential capture next turn
+            // endPit = the pit that the last stone lands in after moving
             int endPit = (pit + b.stonesAt(pit)) % 13;
             return (b.stonesAt(endPit) != 0 && endPit >= 0 + offset && endPit <= 5 + offset) ? b.stonesAt(12 - endPit) : 0;
         }
 
+        // Check for potential go-again for next turn
         public int goAgain(Board b, int pit, int offset)
         {
-            // Check for potential go again for next turn
+            // if the last stone after moving lands in the mancala, return 1
             return (((pit + b.stonesAt(pit)) % 13) == 6 + offset) ? 1 : 0;
         }
 
+        /*
+         * Makes condition checking for minimax cleaner
+         * - if it is my turn, comparison should be maximizing
+         * - if it is opponents turn, comparison should be minimizing
+         */
         public bool miniMaxCompare(int score, int best, bool myTurn)
         {
             return myTurn ? score > best : score < best;
